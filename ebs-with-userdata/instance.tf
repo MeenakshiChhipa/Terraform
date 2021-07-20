4e310ff7b99331ba2dd07d1406efb2650f152266
MeenakshiChhipa <81672141+MeenakshiChhipa@users.noreply.github.com>
2021-07-21 01:26:45 +0530

    Add files via upload

---
+++ ebs-with-userdata/instance.tf
@@ -0,0 +1,24 @@
+provider "aws" {
+  access_key = ""
+  secret_key = ""
+  region     = var.region
+  
+}
+
+resource "aws_instance" "ebs_instance_example" {
+  ami           = lookup(var.ami_id, var.region)
+  instance_type = var.instance_type
+  subnet_id     = aws_subnet.public_1.id
+
+  # Security group assign to instance
+  vpc_security_group_ids = [aws_security_group.allow_ssh.id]
+
+  # key name
+  key_name = var.key_name
+  # User data passing through template rendering
+  user_data = data.template_file.init.rendered
+
+  tags = {
+    Name = "EBS with userdata"
+  }
+}
